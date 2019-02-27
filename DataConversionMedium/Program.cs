d832299e0b27c5ccbc7fcbdc4c6cdd21d1ae9ae5
Akshay <akshaysrikumar@gmail.com>
2019-02-27 06:56:30 -0600

    C#

---
+++ DataConversionMedium/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataConversionMedium
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Implicit Conversion");
+            int i = 345;
+            float f = i;
+
+            Console.WriteLine(f);
+
+            float g = float.Parse("123.212");
+            Console.WriteLine(g);
+            int j = 100;
+            uint u = (uint)j;
+            Console.Write(j);
+        }
+    }
+}
---
+++ DataTypesEasy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string stringVar = "Hello World!!";
+        int intVar = 100;
+        float floatVar = 10.2f;
+        char charVar = 'A';
+        bool boolVar = true;
+        Console.WriteLine(stringVar,intVar,floatVar,charVar,boolVar);
+    }
+}
